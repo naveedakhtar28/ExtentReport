0db03c46318ea3a0ce7ff645c737898c28952f9d
NAVEED0BAB\Naveed <naveed.akhtar@psiog.com>
2018-09-26 17:11:22 +0530

    Staged

--- UnitTestProject2/Utilities.cs
+++ UnitTestProject2/Utilities.cs
@@ -116,11 +116,11 @@ namespace ExtentReportSelenium
             var ExecutionTime = System.Environment.GetEnvironmentVariable("BUILD_TIMESTAMP");
             var ExecutionDate = System.Environment.GetEnvironmentVariable("BUILD_DATE");
             var fileDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Reports");
-            var fileNameMethod = this.GetType().ToString() ;
-            var fileName = ExecutionTime + "_" +fileNameMethod + ".PNG";
-
+            
             if (TestContext.CurrentContext.Result.Outcome.Status != TestStatus.Passed)
             {
+                var fileNameMethod = TestContext.CurrentContext.Test.Name.ToString();
+                var fileName = ExecutionTime + "_" + fileNameMethod + ".PNG";
                 Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
                 ss.SaveAsFile(fileDirectory + "\\" + ExecutionDate + "\\" + fileName , ScreenshotImageFormat.Png);
                 _test.Fail("details").AddScreenCaptureFromPath(fileName);
